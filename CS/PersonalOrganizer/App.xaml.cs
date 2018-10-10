c65d900bec8c84975e368efd4e55625fa125efe7
Martin Brehmer <m.brehmer@zeb-online.de>
2018-10-10 11:06:47 +0200

    [FIX] Upgrade to DevExpress 18.1.6

--- CS/PersonalOrganizer/App.xaml.cs
+++ CS/PersonalOrganizer/App.xaml.cs
@@ -3,10 +3,13 @@ using PersonalOrganizer.Model;
 using System.Data.Entity;
 using System.Windows;
 
-namespace PersonalOrganizer {
-    public partial class App : Application {
-        protected override void OnStartup(StartupEventArgs e) {
-            ThemeManager.ApplicationThemeName = Theme.Office2013.Name;
+namespace PersonalOrganizer
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ApplicationThemeHelper.ApplicationThemeName = Theme.Office2013.Name;
             Database.SetInitializer<ContactContext>(new ContactContextInitializer());
             base.OnStartup(e);
         }
